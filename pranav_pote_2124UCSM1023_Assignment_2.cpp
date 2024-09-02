996d460087215eb39f652b0d8443fefaaaed4b1d
ᴵᴬᴹ  ➳𝙋®ꪖ𝓷ᴧ𝖛➳ <pranav.pote24@sanjivani.edu.in>
2024-09-02 10:25:36 +0530

    Update pranav_pote_2124UCSM1023_Assignment_2.cpp

--- pranav_pote_2124UCSM1023_Assignment_2.cpp
+++ pranav_pote_2124UCSM1023_Assignment_2.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int notebook=15;
     int pen=20;
-   int sum=(notebook*2.25)+(pen*0.75);
+   float sum=(notebook*2.25)+(pen*0.75);
     
     cout<<"total cost of pen and notebook is :"<<sum;
 
